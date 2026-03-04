309c18d94669a84443eb96fcb121296376f378c0
EchosMom <zahrasalie2004@gmail.com>
2026-03-04 09:05:31 +0200

    first commit

---
+++ TCPclient.py
@@ -0,0 +1,17 @@
+"""Sends and recieves"""
+
+import socket
+import threading
+
+serverName = 'OurServer'
+serverPort = 1500
+
+def connect_client():
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSocket.connect((serverName, serverPort))
+
+    threading.Thread(target=receive_message, args=(clientSocket,)).start()
+
+def receive_message(clientSocket):
+    while True:
+        """Receives messages from the server and prints them to the console."""
